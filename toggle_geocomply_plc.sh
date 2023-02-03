fee5b9d97ab8855b5c8804510bafaa6a320d8533
Mike DiDomizio <mikedidomizio@gmail.com>
2023-02-03 05:56:16 -0500

    Update README to have clearer Finder instructions (#1)

--- toggle_geocomply_plc.sh
+++ toggle_geocomply_plc.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# the `launchctl` commands below need to be ran from the directory
+# the `launchctl` commands below need to be run from the directory
 cd /Library/LaunchAgents || exit 1
 
 if [ ! -f com.GeoComply.PlayerLocationCheck.Agent.plist ]
 then
     echo "PLC does not appear to be installed.  It needs to be installed before it can be toggled"
-    exit 0
+    exit 1
 fi
 
 PLC_PID=$(pgrep PlayerLocationCheck)
